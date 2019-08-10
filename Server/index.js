e0a0687755b2a4cc402e2fd4e7fca144638fd137
mille424 <mille424@gmail.com>
2019-08-10 14:42:32 -0400

    added missing files/folders

--- Server/index.js
+++ Server/index.js
@@ -4,11 +4,13 @@ const app = express()
 
 app.use(express.static('client'))
 
-const port = 3004
+const port = 3002
 app.listen(port, () => {
   console.log('listening on port ', port)
 })
 
-app.get('/comments/:artist/:track', function(req, res) {
-  console.log(`GET request for artist ${''} and song ${''}`)
+app.get('/comments/:artist/:song', function(req, res) {
+  console.log(`GET request for artist ${req.params.artist} and song ${'req.params.song'}`)
+
+  res.end()
 })
