021d98ac1f17d2ccd07d00aaf85923058daeac28
mille424 <mille424@gmail.com>
2019-08-10 15:00:30 -0400

    wrote comments Schema

--- database/db.js
+++ database/db.js
@@ -1,2 +1,23 @@
+const mongoose = require('mongoose');
+const mongoUri = 'mongodb://localhost:3002/Soundcloud';
 
+const db = mongoose.connect(mongoUri);
 
+db.on('open',() => console.log('connected to mongoDB'))
+db.on('error', err => {
+  console.log('error connecting to mongoDB ', err)
+})
+
+const commentSchema = new mongoose.Schema({
+  text: {type: String, required: true},
+  songArtist: String,
+  songTitle: String,
+  songTime: String,
+  userName: String,
+  commentDate: Date
+})
+
+const Comments = mongoose.model('Comments', commentSchema)
+
+module.exports.db = db
+module.exports.Comments = Comments
