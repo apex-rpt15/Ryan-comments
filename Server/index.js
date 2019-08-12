9a7da2b430d21b2a59627333306573965715c95e
mille424 <mille424@gmail.com>
2019-08-12 13:58:34 -0400

    outlined testing files, and added placeholders for User data in api

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const select = require('../database/db.js').select;
+const request = require('request')
 
 app.use(express.static('client'));
 app.use(bodyParser.json());
@@ -16,6 +17,23 @@ app.listen(port, () => {
 app.get('/comments/:artist/:song', function(req, res) {
   console.log(`GET request for artist ${req.params.artist} and song ${req.params.song}`)
   select(req.params.artist, req.params.song, (data) => {
-    res.send(data);
+    let results = []
+    request.get('/users', function(error, response, body) {
+
+      //handle list of users from Zack to get the user photos and followers'
+      
+      data.forEach(item => {
+        let comment = {
+          text: item.text,
+          songTime: item.songTime,
+          commentDate: item.commentDate,
+          userName: item.userName,
+          userPhoto: 'someurl.com', //stub until zack's endpoint is available
+          userFollowers: Math.floor(Math.random()) * 1000 //stub until zack's endpoint is available 
+        }
+        results.push(comment)  
+      })    
+      res.send(results)
+    })
   })
 })
---
+++ spec/spec.js
@@ -0,0 +1,25 @@
+const $ = require('jquery')
+const expect = require('chai').expect
+
+
+describe('Database tests', function() {
+
+  it('Database Soundcloud should do something', function(done) {
+    //to do...
+    done();
+  })
+})
+
+describe('Server tests', function() {
+
+  it('Server should do something', function(done) {
+
+    done();
+  })
+})
+
+describe('Client tests', function() {
+  it('Client should do something', function(done) {
+    done();
+  })
+})
