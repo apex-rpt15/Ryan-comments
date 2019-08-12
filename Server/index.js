2e89607e679d83a8a59accab30adb157b66f06e4
mille424 <mille424@gmail.com>
2019-08-12 15:18:36 -0400

    added server tests for GET /comments/:artist/:song

--- Server/index.js
+++ Server/index.js
@@ -16,24 +16,27 @@ app.listen(port, () => {
 
 app.get('/comments/:artist/:song', function(req, res) {
   console.log(`GET request for artist ${req.params.artist} and song ${req.params.song}`)
-  select(req.params.artist, req.params.song, (data) => {
-    let results = []
-    request.get('/users', function(error, response, body) {
-
-      //handle list of users from Zack to get the user photos and followers'
-      
-      data.forEach(item => {
-        let comment = {
-          text: item.text,
-          songTime: item.songTime,
-          commentDate: item.commentDate,
-          userName: item.userName,
-          userPhoto: 'someurl.com', //stub until zack's endpoint is available
-          userFollowers: Math.floor(Math.random()) * 1000 //stub until zack's endpoint is available 
-        }
-        results.push(comment)  
-      })    
-      res.send(results)
+  let results = []
+  if(!(req.params.artist || req.params.song)) {
+    console.log('artist or song not specified')
+    res.send(results)
+  } else {
+    select(req.params.artist, req.params.song, (data) => { 
+      request.get('/users', function(error, response, body) {
+        //handle list of users from Zack to get the user photos and followers'
+        data.forEach(item => {
+          let comment = {
+            text: item.text,
+            songTime: item.songTime,
+            commentDate: item.commentDate,
+            userName: item.userName,
+            userPhoto: 'someurl.com', //stub until zack's endpoint is available
+            userFollowers: Math.floor(Math.random()) * 1000 //stub until zack's endpoint is available 
+          }
+          results.push(comment)  
+        })    
+        res.send(results)
+      })
     })
-  })
+  }
 })
--- spec/spec.js
+++ spec/spec.js
@@ -1,5 +1,6 @@
 const $ = require('jquery')
 const expect = require('chai').expect
+const request = require('request')
 
 
 describe('Database tests', function() {
@@ -10,16 +11,50 @@ describe('Database tests', function() {
   })
 })
 
+
 describe('Server tests', function() {
 
-  it('Server should do something', function(done) {
-
-    done();
+  it('should return status code 200 for a valid request (Artist: AmigoKing Song: Little Bug)', function(done) {
+    request.get('http://localhost:3002/comments/AmigoKing/Little%20Bugs', function(error, response, body){
+      expect(response.statusCode).to.equal(200);
+      done();
+    })  
   })
+
+  it('should return 100 comments for test song (Artist: AmigoKing Song: Little Bug)', function(done){
+    request.get('http://localhost:3002/comments/AmigoKing/Little%20Bugs', function(error, response, body){
+      let parsedBody = JSON.parse(body);
+      expect(parsedBody.length).to.equal(100);
+      done();
+    })  
+  })
+
+  it('should respond with status code 404 if artist and song are not specified', function(done) {
+    request.get('http://localhost:3002/comments', function(error, response, body) {
+      expect(response.statusCode).to.equal(404)
+      done();
+    })
+  })
+
+  it('should have the correct properties in the response objects', function(done) {
+    request.get('http://localhost:3002/comments/AmigoKing/Little%20Bugs', function(error, response, body){
+      let parsedBody = JSON.parse(body);
+      console.log(parsedBody[0])
+      expect(parsedBody[0].text).to.exist;
+      expect(parsedBody[0].songTime).to.exist;
+      expect(parsedBody[0].commentDate).to.exist;
+      expect(parsedBody[0].userName).to.exist;
+      expect(parsedBody[0].userPhoto).to.exist;
+      expect(parsedBody[0].userFollowers).to.exist;
+      done();
+    })  
+  })
+
 })
 
 describe('Client tests', function() {
   it('Client should do something', function(done) {
+    //To do...
     done();
   })
 })
